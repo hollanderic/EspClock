195ee449130cecbbb52194f95a8e975c844e9091
Eric Holland <eric.holland@gmail.com>
2026-03-09 18:43:09 -0400

    fix path option

--- setup_clangd.py
+++ setup_clangd.py
@@ -2,7 +2,7 @@ import os
 Import("env")
 
 # Include toolchain paths in the compilation database
-env.Replace(COMPILATIONDB_INCLUDE_TOOLCHAIN=True)
+env.Replace(COMPILATIONDB_INCLUDE_TOOLCHAIN=False)
 
 # Optional: Override the path to ensure it's in the root
 env.Replace(COMPILATIONDB_PATH="compile_commands.json")
